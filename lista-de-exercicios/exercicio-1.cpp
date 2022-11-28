43d721fbf5fd671e3fa3a26dbe2a2b6ace160691
gusBrasil <gustavomedbra@gmail.com>
2022-11-28 19:38:43 -0300

    update

--- lista-de-exercicios/exercicio-1.cpp
+++ lista-de-exercicios/exercicio-1.cpp
@@ -4,14 +4,20 @@
 // formato “base  expoente”. Use para a solução deste problema a técnica de loop com o
 // conceito acumulador / contador.
 
+// Programa feito por Gustavo Medeiros Brasil para a matéria de bases de programação
+
 #include <iostream>
 using namespace std;
 
-int B, E;
+int B = 0, E = 0, i = 0, resultado = 0;
 
 int main() {
-    cout << "Insira o valor base: " << "\n"; cin >> B;
-    cout << "Insira o valor expoente: " << "\n"; cin >> E;
-    int resultado = B^E;
-    cout << resultado;
+    cout << "Determine o valor da base: "; cin >> B;
+    cout << "\nDetermine o valor do expoente: "; cin >> E;
+
+    for (i = 1; i <= E; i++){
+        resultado = resultado * B;
+    }
+
+    cout << "A resposta é: " << resultado;
 }
