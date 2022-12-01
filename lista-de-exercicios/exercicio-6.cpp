939dca164644039875d3fb9753c1d6e790ace90e
Gustavo Brasil <64227804+gusBrasil@users.noreply.github.com>
2022-12-01 11:11:42 -0300

    Update exercicio-6.cpp

--- lista-de-exercicios/exercicio-6.cpp
+++ lista-de-exercicios/exercicio-6.cpp
@@ -2,36 +2,43 @@
 #include <vector>
 using namespace std;
 
-int A [12] = {1,2,3,4,5,6};
-int B [12] = {1,2,3,4,5,6};
-int C [24] = {1,2,3,4,5,6};
+int A [12];
+int B [12];
+int C [24];
 int i;
 bool certo;
 
 int main(){
-    for(i = 1; i <= 12; i++){
-        while(certo <- false){
-                cout << "Digite o " << i << "º valor: ";
-                cin >> A[i];
-                if (A[i] % 2 == 0 || A[i] % 3 == 0){
-                C[i] <- A[i];
-                certo <- true;
-            }
+    i=1;
+    do{
+        cout << "Digite um valor para a posiÃ§Ã£o " << i << "\n ";
+        cin >> A[i];
+        if (A[i] % 2 == 0 || A[i] % 3 == 0){
+            i=i+1;
         }
-    }
-    for(i = 1; i <= 12; i++){
-        while (certo <- false){
-            cout << "Digite o " << i << "º valor: ";
-            cin >> B[i];
-            if (B[i] != 5){
-                C[i + 12] <- B[i];
-                certo <- true;
-            
-            }
+        else {
+            cout << "Valor invÃ¡lido, tente novamente.";
         }
+    }while (i<=12);
+    
+    i=1;
+    
+    while (i<=12){
+        cout << "Digite um valor para a posiÃ§Ã£o " << i << "\n ";
+        cin >> B[i];
+        if(B[i] % 5 != 0) {
+            i = i+1;
+        }
+        else {
+                cout << "Valor invÃ¡lido, digite novamente.";
+            }
     }
-    cout << "Os valores lidos da matriz C: ";
-    for(i = 1; i <= 24; i++){
-        cout << i/2 << "º é: " << C[i]/3;
+    
+    for(i = 0; i < 12; i++) {
+        C[i] = A[i];
+        C[i+12] = B[i];
     }
+	for(i=0; i<24; i++){
+		cout << C[i] << "\n";
+	}    
 }
